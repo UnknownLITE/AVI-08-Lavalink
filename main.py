ac7bd2aa14cdaffdd9a38aee709a39dfd946535b
UnknownLITE <91004913+UnknownLITE@users.noreply.github.com>
2021-10-06 21:55:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import time
-from jdk.bin.setup_bot import setup
 from bot import MusicBot
 
 
 def main():
     bot = MusicBot()
-    time.sleep(10)
+    time.sleep(50)
     bot.run()
 
 
 if __name__ == "__main__":
-    setup()
     main()
